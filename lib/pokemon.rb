c6e0e3b79174c3195b6e2cb1ee6e467c0e68fff5
John Hayes <johnnyhayes7@gmail.com>
2019-03-25 13:55:55 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,7 @@ class Pokemon
  end
  
  def self.find(id, db)
-   pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", [id]);
+   pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id);
    
    n_poke = self.new(pokemon)
    
